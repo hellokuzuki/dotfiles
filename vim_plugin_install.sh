94a5bc678e29a15da4005e7dcd8797813ba89bbb
hellokuzuki <miximixi_wang@hotmail.com>
2019-01-09 15:02:58 +1100

    add more tools

--- vim_plugin_install.sh
+++ vim_plugin_install.sh
@@ -1,10 +1,22 @@
 #! /bin/bash
 
-ctrlp="/home/$USER/.vim/bundle/ctrlp.vim"
-#Install CtrlP
-if [ ! -d $ctrlp ]; then
-	echo "Installing Ctrlp plugin..."
-	git clone https://github.com/kien/ctrlp.vim.git $ctrlp
+# Install Vundle
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 else
-	echo "Ctrlp does exist, skip install."
+	echo "*** Vundle.vim doses already exist. Skip install Vundle plugin"
+fi
+
+# Install CtrlP
+if [ ! -d $HOME/.vim/bundle/ctrlp.vim ]; then
+	git clone https://github.com/kien/ctrlp.vim.git $HOME/.vim/bundle/ctrlp.vim
+else
+	echo "*** CtrlP does already exist. Skip install CtrlP plugin"
+fi
+
+# Install YouCompleteMe
+if [ ! -d $HOME/.vim/bundle/YouCompleteMe ]; then
+	git clone https://github.com/Valloric/YouCompleteMe $HOME/.vim/bundle/YouCompleteMe/
+else
+	echo "*** YouCompleteMe does already exist. Skip install plugin"
 fi
